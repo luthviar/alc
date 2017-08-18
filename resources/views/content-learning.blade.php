971986daed97416345c7e8dbfcc615604d87b143
rohmattaufik <rohmat.taufik14@gmail.com>
2017-08-18 13:33:03 +0700

    Dina FS

--- resources/views/content-learning.blade.php
+++ resources/views/content-learning.blade.php
@@ -34,10 +34,11 @@
             <ul class="nav navbar-nav pull-right" style="cursor: pointer;">
                 <!-- BEGIN USER LOGIN DROPDOWN -->
                 @if (Auth::guest()) 
-                    <li><a style="margin-top:10px;" class="btn btn-small btn-sm pull-right hijau-muda" href="{{ route('login') }}">Login <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i> </a></li>
+                    <li>
+                    	<a style="margin-top:10px;" class="btn btn-small btn-sm pull-right hijau-muda" href="{{ route('login') }}">Login <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i> </a>
+                    </li>
                 @else
                     <li class="dropdown user">
-                         
                         <a class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
                             <img alt="" src="assets/img/avatar1_small.jpg"/>
                             <span class="username hidden-1024">{{Auth::user()->get_nama()}}</span>
@@ -80,14 +81,19 @@
 							 Home
 						</a>
 					</li>
-					<li class="classic-menu-dropdown"><a href="/news-board">News</a></li>
+					<li class="classic-menu-dropdown">
+						<a href="/news-board">News</a>
+					</li>
 					@if(Auth::user())
-					<li class="classic-menu-dropdown"><a href="{{url('/forum')}}">Forum</a></li>
-					<li class="classic-menu-dropdown active"><a data-toggle="dropdown" data-hover="dropdown" data-close-others="true" href="#">
+					<li class="classic-menu-dropdown">
+						<a href="{{url('/forum')}}">Forum</a>
+					</li>
+					<li class="classic-menu-dropdown active">
+						<a data-toggle="dropdown" data-hover="dropdown" data-close-others="true" href="#">
 							My Modules <i class="fa fa-angle-down"></i>
 							<span class="selected">
 							</span>
-						</a><
+						</a>
 						<ul class="dropdown-menu">
 							@foreach ($module as $modul)
 								<li>
@@ -139,7 +145,7 @@
 
         </div>
        <div class="page-content-wrapper"> 
-			@if(empty($logout))
+	@if(empty($logout))
             
         <div class="page-content" style="background-color: rgb(243, 247, 248);opacity: 1;">	
             <div class="container">
@@ -156,17 +162,17 @@
 			</div>
 			<br>
 			<div class="block-advice">
-					<div class = "text-center">
+				<div class = "text-center">
 					<h2 class="brand-before">
-							<small>{{$training->title}}</small>
-						</h2>
-						<h1 class="brand-name">Materi Training</h1>
+						<small>{{$training->title}}</small>
+					</h2>
+					<h1 class="brand-name">Materi Training</h1>
 
-						<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+					<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 						@foreach($content as $materi)
-						  <div class="panel panel-default">
+						<div class="panel panel-default">
 						    <div class="panel-heading" role="tab" id="headingOne">
-						      <h4 class="panel-title">
+						      	<h4 class="panel-title">
 						      		{{$materi->file_name}}<br>
 						      	</h4>
 						    </div>
@@ -175,37 +181,23 @@
 						      	<div class="row">
 						      		<a href="{{URL::asset($materi->url)}}">File_Materi</a>
 						      	</div>
-						      	
 						    </div>  
-						  </div>
-						  
-						  @endforeach
-						</div>
-							
-						
-						<br>
-						<h6>
-							
-							<a class="btn darkgreen" href="/section-training/{{$next_section->id}}">Next</a>
-							
-							
-						</h6>
-						
-						
+						</div>  
+						@endforeach
 					</div>
+					<br>
+					<h6>
+						<a class="btn darkgreen" href="/section-training/{{$next_section->id}}">Next</a>
+					</h6>
 				</div>
-                
-            </div>
-            
-			
+			</div>
+        </div>
+        	
 	@else
-
-
-			
-        <div class="page-content" style="background-color: rgb(243, 247, 248);opacity: 1;">	
+		
+		<div class="page-content" style="background-color: rgb(243, 247, 248);opacity: 1;">	
             <div class="container">
 				<div class="row">
-				
 					<div class="btn-group btn-breadcrumb">
 						<a  class="btn btn-default">Introduction</a>
 						<a  class="btn btn-default">Pre-Test</a>
@@ -213,69 +205,43 @@
 						<a  class="btn btn-default">Post-Test</a>
 					</div>
 				</div>
-	   
-			</div>
+	   		</div>
 			<br>
 			<div class="block-advice">
-					<div class = "text-center">
+				<div class = "text-center">
 					<h2 class="brand-before">
-							<small>{{$training->title}}</small>
-						</h2>
-						<h1 class="brand-name">Materi Training</h1>
+						<small>{{$training->title}}</small>
+					</h2>
+					<h1 class="brand-name">Materi Training</h1>
 
-						<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+					<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 						@foreach($content as $materi)
-						  <div class="panel panel-default">
+						<div class="panel panel-default">
 						    <div class="panel-heading" role="tab" id="headingOne">
-						      <h4 class="panel-title">
-						        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne{{$materi->id}}" aria-expanded="false" aria-controls="collapseOne">
-						          {{$materi->file_name}}
-						        </a>
-						      </h4>
+						      	<h4 class="panel-title">
+						      		{{$materi->file_name}}<br>
+						      	</h4>
 						    </div>
-						    <div id="collapseOne{{$materi->id}}" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
-
-						      <div class="panel-body">
-								  {{--<iframe src="http://localhost:8000/Uploads/Situs%20Resmi%20PT.pdf?page=hsn#toolbar=0"--}}
-										  {{--width="720" height="600" style="border: none;" type="text/html">--}}
-								  {{--</iframe>--}}
-								  {{--<iframe--}}
-
-										  {{--src="http://docs.google.com/gview?url=http://localhost:8000/Uploads/Situs%20Resmi%20PT.pdf&embedded=true"--}}
-										  {{--style="width:100%; height:1000px;" frameborder="0"></iframe>--}}
-						        <iframe id="viewer"
-										src = "{{URL::to($materi->url)}}"
-										width='100%'
-										height='600'
-										allowfullscreen webkitallowfullscreen>
-								</iframe>
-
-						      </div>
-						    </div>
-						  </div>
-						  @endforeach
-						</div>
-							
-						
-						<br>
-						<h6>
-							
-							<a class="btn btn-warning" href="/module/{{$training->id_module}}">Back</a>
-							<a class="btn darkgreen" href="/section-training/{{$next_section->id}}">See Result</a>
-							
-							
-						</h6>
-						
-						
-					</div>
+						    <div>
+						      	<p>{!! html_entity_decode($materi->description) !!}</p>
+						      	<div class="row">
+						      		<a href="{{URL::asset($materi->url)}}">File_Materi</a>
+						      	</div>
+						    </div>  
+						</div>  
+						@endforeach
+					</div>				
+					<br>
+					<h6>
+						<a class="btn btn-warning" href="/module/{{$training->id_module}}">Back</a>
+						<a class="btn darkgreen" href="/section-training/{{$next_section->id}}">See Result</a>
+					</h6>
 				</div>
-                
-            </div>
-            
-				
-			@endif
-		</div>
-	   <!-- Footer -->
+			</div>
+        </div>
+		@endif
+	</div>
+	 <!-- Footer -->
         @include('layouts.footer')
     </div>
 
