710b7072c7e234277c085f8716fde2c86b5d2f62
rohmattaufik <rohmat.taufik14@gmail.com>
2017-08-18 15:23:03 +0700

    sadasdsa

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -1,4 +1,4 @@
-<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+
 <script type="text/javascript" src="{{URL::asset('Elegantic/js/jquery.bxslider.min.js')}}"></script>
 <script type="text/javascript" src="{{URL::asset('Elegantic/js/jquery.placeholder.js')}}"></script>
 <script type="text/javascript" src="{{URL::asset('Elegantic/js/jquery.uniform.min.js')}}"></script>
@@ -6,3 +6,4 @@
 <script type="text/javascript" src="{{URL::asset('Elegantic/js/main.js')}}"></script>
 <script type="text/javascript" src="{{URL::asset('Elegantic/js/app.js')}}"></script>
 <!-- END CORE PLUGINS -->
+
